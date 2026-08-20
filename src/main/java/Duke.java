a1395ecaa91ccce18a24a293eae9b5f7333f142e
wellbakedquiche <keisha.anne.robles@gmail.com>
2026-08-21 00:03:56 +0800

    Added echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         greet();
+        String input = get_input();
+        while (!input.equals("bye")) {
+            echo(input);
+            input = get_input();
+        }
         exit();
     }
 
-    public static void greet() {
+    private static void greet() {
         String banner =
                 " ______                ______               \n" +
                         " | ___ \\               | ___ \\              \n" +
@@ -14,10 +21,21 @@ public class Duke {
                         " \\____/ \\___/|_| |_|   \\____/ \\___/|_| |_|  \n";
         System.out.println(banner);
         System.out.println("Hi! I'm BonBon");
-        System.out.println("What can I do for you?\n");
+        System.out.println("What're we doing today?\n");
     }
 
-    public static void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
+    private static void exit() {
+        System.out.println("Bye bye!!!!");
+    }
+
+    private static String get_input() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return input;
+    }
+
+    private static void echo(String str) {
+        System.out.println(str);
+        System.out.println();
     }
 }
