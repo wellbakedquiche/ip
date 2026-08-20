9d7e331ac6a06232af63cb91fa3166e7733b1810
wellbakedquiche <keisha.anne.robles@gmail.com>
2026-08-20 23:36:39 +0800

    Changed name and added greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        greet();
+        exit();
+    }
+
+    public static void greet() {
+        String banner =
+                " ______                ______               \n" +
+                        " | ___ \\               | ___ \\              \n" +
+                        " | |_/ / ___  _ __     | |_/ / ___  _ __    \n" +
+                        " | ___ \\/ _ \\| '_ \\    | ___ \\/ _ \\| '_ \\   \n" +
+                        " | |_/ / (_) | | | |   | |_/ / (_) | | | |  \n" +
+                        " \\____/ \\___/|_| |_|   \\____/ \\___/|_| |_|  \n";
         System.out.println(banner);
+        System.out.println("Hi! I'm BonBon");
+        System.out.println("What can I do for you?\n");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
